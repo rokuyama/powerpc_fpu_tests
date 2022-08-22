fd81f6e2303666ee6ca6f54779ce24f103e5e969
Rin Okuyama <rokuyama.rk@gmail.com>
2022-08-22 21:04:41 +0900

    Import initial version.

---
+++ fpu.c
@@ -0,0 +1,186 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "fpu.h"
+
+#define	TEST_FPSCR(func, expected, ignored)				\
+    test_fpscr(func, expected, ignored, #func)
+
+static void
+test_fpscr(uint32_t (*func)(void), uint32_t expected, uint32_t ignored,
+    const char *name)
+{
+	uint32_t mask, result;
+
+	mask = 0xffffffffU & ~ignored;
+
+	set_fpscr(false, FPSCR_MCRFS_MASK | FPSCR_FPRF);
+	set_fpscr(true, FPSCR_ENABLE_MASK);
+	set_msr(false, MSR_FE_MASK);
+
+	result = func();
+	if ((result & mask) != (expected & mask)) {
+		printf("%s: FAILED\n", name);
+		printf("\texpected 0x%08x (0x%08x)\n",
+		    expected, expected & mask);
+		printf("\tresult   0x%08x (0x%08x)\n",
+		    result, result & mask);
+	} else {
+		printf("%s: passed 0x%08x (0x%08x)\n",
+		    name, result, result & mask);
+	}
+}
+
+#define	TEST_RD(func, expected)	test_rd(func, expected, #func)
+
+static void
+test_rd(double (*func)(void), double expected, const char *name)
+{
+	double result;
+
+	set_fpscr(false, FPSCR_MCRFS_MASK | FPSCR_FPRF);
+	set_fpscr(false, FPSCR_ENABLE_MASK);
+	set_msr(false, MSR_FE_MASK);
+
+	result = func();
+	if (result != expected) {
+		printf("%s: FAILED %f != %f\n", name, result, expected);
+	} else {
+		printf("%s: passed %f\n", name, result);
+	}
+}
+
+static uint32_t
+raise_zx(void)
+{
+	volatile double a = atoi("1"), b = atoi("0"), d;
+	fp fpscr;
+
+	asm __volatile (
+		"fdiv	%[d],%[a],%[b];"
+		"mffs	%[fpscr];"
+		: [d] "=f" (d), [fpscr] "=f" (fpscr.fp)
+		: [a] "f" (a), [b] "f" (b)
+	);
+
+	return fpscr.word[1];
+}
+
+static uint32_t
+fneg(void)
+{
+	volatile double a = 1.0, d;
+	fp fpscr;
+
+	asm __volatile (
+		"fneg	%[d],%[a];"
+		"mffs	%[fpscr];"
+		: [d] "=f" (d), [fpscr] "=f" (fpscr.fp)
+		: [a] "f" (a)
+	);
+
+	return fpscr.word[1];
+}
+
+static uint32_t
+fadd(void)
+{
+	volatile double a = 1.0, b = 1.0, d;
+	fp fpscr;
+
+	asm __volatile (
+		"fadd	%[d],%[a],%[b];"
+		"mffs	%[fpscr];"
+		: [d] "=f" (d), [fpscr] "=f" (fpscr.fp)
+		: [a] "f" (a), [b] "f" (b)
+	);
+
+	return fpscr.word[1];
+}
+
+static uint32_t
+fsel_qnan_s(void)
+{
+	volatile double c = 1.0, b = 0.0, d;
+	fp a, fpscr;
+
+	a.bin = FP_QNAN;
+	asm __volatile (
+		"fsel	%[d],%[a],%[c],%[b];"
+		"mffs	%[fpscr];"
+		: [d] "=f" (d), [fpscr] "=f" (fpscr.fp)
+		: [a] "f" (a), [c] "f" (c), [b] "f" (b)
+	);
+
+	return fpscr.word[1];
+}
+
+static double
+fsel(uint64_t bin)
+{
+	volatile double c = 1.0, b = 0.0, d;
+	volatile fp a;
+
+	a.bin = bin;
+	asm __volatile (
+		"fsel	%[d],%[a],%[c],%[b];"
+		: [d] "=f" (d)
+		: [a] "f" (a.fp), [c] "f" (c), [b] "f" (b)
+	);
+
+	return d;
+}
+
+static double
+fsel_snan(void)
+{
+
+	return fsel(FP_SNAN);
+}
+
+static double
+fsel_qnan(void)
+{
+
+	return fsel(FP_QNAN);
+}
+
+static double
+fsel_mzero(void)
+{
+
+	return fsel(FP_MZERO);
+}
+
+static void
+set_fx(void)
+{
+
+	set_fpscr(false, FPSCR_MCRFS_MASK | FPSCR_FPRF);
+	set_fpscr(true, FPSCR_ENABLE_MASK);
+	set_msr(false, MSR_FE_MASK);
+
+	set_fpscr(true, FPSCR_FX);
+
+	printf("%s: passed\n", __func__);
+}
+
+int
+main(void)
+{
+
+	/* expected values were taken from MPC8245 (603e) */
+
+	TEST_FPSCR(raise_zx,	0xc40020f8, FPSCR_FPRF);
+/*ok*/	TEST_FPSCR(fneg,	0x000000f8, 0);
+	TEST_FPSCR(fadd,	0x000040f8, 0);
+/*ok*/	TEST_FPSCR(fsel_qnan_s,	0x000000f8, FPSCR_FPRF);
+
+	TEST_RD(fsel_snan,	0.0);
+	TEST_RD(fsel_qnan,	0.0);
+/*ok*/	TEST_RD(fsel_mzero,	1.0);
+
+/*ok*/	set_fx();
+
+	return 0;
+}
---
+++ fpu.h
@@ -0,0 +1,174 @@
+#include <sys/types.h>
+#include <sys/cdefs.h>
+
+#include <stdbool.h>
+
+/* Double-Precision in FPR */
+
+#define	FP_MASK_SIGN	__BIT(63)
+#define	FP_MASK_EXP	__BITS(52, 62)
+#define	FP_MASK_FRAC	__BITS(0, 51)
+
+#define	FP_PZERO	0ULL
+#define	FP_MZERO	(FP_MASK_SIGN | FP_PZERO)
+
+#define	FP_PINF		FP_MASK_EXP
+#define	FP_MINF		(FP_MASK_SIGN | FP_PINF)
+
+#define	FP_SNAN		(FP_MASK_EXP | __BITS(0, 51))	/* XXX */
+#define	FP_QNAN		(FP_MASK_EXP | __BITS(0, 50))	/* XXX */
+
+/* Single-Precision in FPR */
+
+#define	SFP_MASK_SIGN	FP_MASK_SIGN
+#define	SFP_MASK_EXP	FP_MASK_EXP			/* XXX */
+#define	SFP_MASK_FRAC	__BITS(29, 51)
+
+#define	SFP_PZERO	0ULL
+#define	SFP_MZERO	(SFP_MASK_SIGN | SFP_PZERO)
+
+#define	SFP_PINF	SFP_MASK_EXP
+#define	SFP_MINF	(SFP_MASK_SIGN | SFP_PINF)
+
+#define	SFP_SNAN	(SFP_MASK_EXP | __BITS(29, 51)) /* XXX */
+#define	SFP_QNAN	(SFP_MASK_EXP | __BITS(29, 50))	/* XXX */
+
+/* FPSCR bits */
+
+#define	FPSCR_FX	__BIT(31)	/* s */
+#define	FPSCR_FEX	__BIT(30)	/* or */
+#define	FPSCR_VX	__BIT(29)	/* or */
+
+#define	FPSCR_OX	__BIT(28)
+#define	FPSCR_UX	__BIT(27)
+#define	FPSCR_ZX	__BIT(26)
+#define	FPSCR_XX	__BIT(25)
+
+#define	FPSCR_VXSNAN	__BIT(24)
+#define	FPSCR_VXISI	__BIT(23)
+#define	FPSCR_VXIDI	__BIT(22)
+#define	FPSCR_VXZDZ	__BIT(21)
+#define	FPSCR_VXIMZ	__BIT(20)
+#define	FPSCR_VXVC	__BIT(19)
+
+#define	FPSCR_FR	__BIT(18)
+#define	FPSCR_FI	__BIT(17)
+#define	FPSCR_FPRF	__BITS(12, 16)
+#define	 FPRF_C		__BIT(16)
+#define	 FPRF_FL	__BIT(15)
+#define	 FPRF_FG	__BIT(14)
+#define	 FPRF_FE	__BIT(13)
+#define	 FPRF_FU	__BIT(12)
+#define	  FPRF_QNAN	(FPRF_C | FPRF_FU)
+#define	  FPRF_MINF	(FPRF_FL | FPRF_FU)
+#define	  FPRF_MNOR	FPRF_FL
+#define	  FPRF_MDEN	(FPRF_C | FPRF_FL)
+#define	  FPRF_MZERO	(FPRF_C | FPRF_FE)
+#define	  FPRF_PZERO	FPRF_FE
+#define	  FPRF_PDEN	(FPRF_C | FPRF_FG)
+#define	  FPRF_PNOR	FPRF_FG
+#define	  FPRF_PINF	(FPRF_FG | FPRF_FU)
+
+#define	FPSCR_RSVD	__BIT(11)
+
+#define	FPSCR_VXSOFT	__BIT(10)
+#define	FPSCR_VXSQRT	__BIT(9)
+#define	FPSCR_VXCVI	__BIT(8)
+
+#define	FPSCR_VE	__BIT(7)
+#define	FPSCR_OE	__BIT(6)
+#define	FPSCR_UE	__BIT(5)
+#define	FPSCR_ZE	__BIT(4)
+#define	FPSCR_XE	__BIT(3)
+
+#define	FPSCR_NI	__BIT(2)
+
+#define	FPSCR_RN	__BIT(1, 0)
+#define	 RN_NEAR	0x0ULL
+#define	 RN_ZERO	0x1ULL
+#define	 RN_PINF	0x2ULL
+#define	 RN_MINF	0x3ULL
+
+#define	FPSCR_ENABLE_MASK						\
+    (FPSCR_VE | FPSCR_OE | FPSCR_UE | FPSCR_ZE | FPSCR_XE)
+
+#define	FPSCR_MCRFS_MASK						\
+    (									\
+	FPSCR_FX     | FPSCR_OX     | 					\
+	FPSCR_UX     | FPSCR_ZX     | FPSCR_XX    | FPSCR_VXSNAN | 	\
+	FPSCR_VXISI  | FPSCR_VXIDI  | FPSCR_VXZDZ | FPSCR_VXIMZ  |	\
+	FPSCR_VXVC   | 							\
+	FPSCR_VXSOFT | FPSCR_VXSQRT | FPSCR_VXCVI			\
+    )
+
+/* MSR bits */
+#define	MSR_FE0		__BIT(11)
+#define	MSR_FE1		__BIT(8)
+#define	MSR_FE_MASK	(MSR_FE0 | MSR_FE1)
+
+typedef union fp {
+	double fp;
+	uint64_t bin;
+	uint32_t word[2];
+} fp;
+
+static inline uint32_t
+mfmsr(void)
+{
+	uint32_t msr;
+
+	__asm volatile ("mfmsr %0" : "=r"(msr));
+	return msr;
+}
+
+static inline uint32_t
+mtmsr(uint32_t msr)
+{
+
+	__asm volatile ("mtmsr %0" : : "r"(msr));
+}
+
+static inline uint32_t
+mffs(void)
+{
+	fp fpscr;
+
+	__asm volatile("mffs %0" : "=f"(fpscr.fp));
+	return fpscr.word[1];
+}
+
+static inline void
+mtffs(uint32_t bits)
+{
+	fp fpscr;
+
+	fpscr.word[0] = 0;
+	fpscr.word[1] = bits;
+	__asm __volatile("mtfsf 0xff,%0" : : "f"(fpscr.fp));
+}
+
+static inline void
+set_msr(bool on, uint32_t mask)
+{
+	uint32_t msr;
+
+	msr = mfmsr();
+	if (on)
+		msr |= mask;
+	else
+		msr &= ~mask;
+	mtmsr(msr);
+}
+
+static inline void
+set_fpscr(bool on, uint32_t mask)
+{
+	uint32_t fpscr;
+
+	fpscr = mffs();
+	if (on)
+		fpscr |= mask;
+	else
+		fpscr &= ~mask;
+	mtffs(fpscr);
+}
